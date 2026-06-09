50a9432a0025b74f0679279ba719314bddf4913d
MikeHiciano <mikewashere@outlook.es>
2026-06-09 14:24:02 -0400

    adding paho mqtt and psicopg

--- server.py
+++ server.py
@@ -1,13 +1,8 @@
-#import paho.mqtt.client as mqtt
+import paho.mqtt.client as mqtt
 from flask import Flask, jsonify
 import requests
 
-app = Flask(__name__)
-
-# @app.errorhandler('404')
-# def errorhandler():
-#     req = requests.get('https://naas.isalman.dev/no')
-#     return req.json()['reason'], 404
+app = App(__name__)
 
 @app.route('/')
 def main():
@@ -17,6 +12,14 @@ def main():
 def main_route():
     req = requests.get('https://naas.isalman.dev/no')
     return req.json()['reason']
+
+@app.route('/message')
+def message():
+    client = mqtt.Client()
+    client.connect('bonsamie-mqtt', 1883, 60)
+    client.publish('topic/test', 'Hello MQTT')
+    client.disconnect()
+        return 'Message sent to MQTT broker'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
