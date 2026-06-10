073511b4aece11ee22c01e2cabfb668439bd0afe
MikeHiciano <mikewashere@outlook.es>
2026-06-10 13:14:01 -0400

    [ADD] adding secret and database functions

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
+from dotenv import load_dotenv
 import paho.mqtt.client as mqtt
 from flask import Flask, jsonify
+import psycopg2
 import requests
+import os
 
+load_dotenv()
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,9 +21,23 @@ def main_route():
 def message():
     client = mqtt.Client()
     client.connect('test.mosquitto.org', 1883, 60)
-    client.publish('topic/testm', 'Hello MQTT')
+    client.publish(os.getenv('mqtt_topic'), 'Hello MQTT')
     client.disconnect()
     return 'Message sent to MQTT broker'
+
+@app.route('/db')
+def db():
+    conn = psycopg2.connect(
+        host=os.getenv('DB_HOST'),
+        database=os.getenv('DB_NAME'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD')
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM users;")
+    rows = cur.fetchall()
+    conn.close()
+    return jsonify(rows)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
